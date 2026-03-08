46636b3b3bb682fc610b6243dbec1e60c932555e
Ilhan Negis <ilhan.negis@gmail.com>
2026-03-08 16:19:53 +0300

    examples fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./*.html", "./partials/**/*.html"],
+  safelist: ["bg-amber-400/20"],
   theme: {
     extend: {
       colors: {
